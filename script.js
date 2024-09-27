17d6aba1a8f29e44e58efa0ffd83f8f9e30a312d
Janderson <jandersonsouza_phn@hotmail.com>
2024-09-27 10:59:56 -0300

    segundaAlteracao

---
+++ script.js
@@ -0,0 +1,12 @@
+
+        let numero = 0;
+        
+        function aumentar() {
+            numero++;
+            document.getElementById("contador").textContent = numero;
+        }
+        
+        function diminuir() {
+            numero--;
+            document.getElementById("contador").textContent = numero;
+        }
